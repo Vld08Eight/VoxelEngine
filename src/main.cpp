72c46e98bb88fb364558ee0565d26b21a76b5178
Vld08Eight <vld088mail@gmail.com>
2024-01-26 22:18:04 +0300

    added onScreen method

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,32 @@ Vector3 cubeSize = { 1.0f, 1.0f, 1.0f };
 RayCollision collision = { 0 };
 int i,j,k,s;
 float x = 0,y = 0,z = 0;
-int arr[100][100][200] = {0};//curPos = (Vector3){0,0,0};
+int arr[1000][1000][200] = {0};//curPos = (Vector3){0,0,0};
 Ray ray[960][540] = { 0 };  
 int buffer[960][540] = {0};
 int w = 960, h = 540;
 Vector2 draws[7];
 //Vector3 dots3D[8] = {0};
 Vector3 cubes[8];
-
+Vector3 v1 = {0}, v2 = {0};
 
 //Vector2 GetWorldToScreen = {0};
 static void UpdateDrawFrame(void);
+
+int getSign(int a){
+    if (a < 0)return -1;
+    else return 1;
+}
+
+bool onScreen(Vector3 dot3D){
+    Vector3 v1 = camera.position;
+    Vector3 v2 = camera.target;
+    Vector3 v12 = (Vector3){camera.target.x - camera.position.x,camera.target.y - camera.position.y,camera.target.z - camera.position.z};
+    Vector3 v1d = (Vector3){dot3D.x - camera.position.x,dot3D.y - camera.position.y,dot3D.z - camera.position.z};
+    float ang = acos((float)(v12.x*v1d.x + v12.y*v1d.y + v12.z*v1d.z)/((float)sqrt(v12.x*v12.x + v12.y*v12.y + v12.z*v12.z) * (float)sqrt(v1d.x*v1d.x + v1d.y*v1d.y + v1d.z*v1d.z)))*180/3.1415;
+    if (ang <= 60)return true;
+    else return false;
+}
 
 float objectDistance(Vector3 vec1, Vector3 vec2){
     return sqrt(pow(vec1.x - vec2.x,2)+pow(vec1.y - vec2.y,2)+pow(vec1.z - vec2.z,2));
@@ -63,6 +78,14 @@ Vector3* getDots(int x, int y, int z){
 }
 
 void DrawCW(Vector3 dots3D[]){
+    bool draw = true;
+    for (int i = 0; i < 8; i++){
+        if (!onScreen(dots3D[i])){
+            draw = false;
+            break;
+        }
+    }
+    if (draw){
     int h = GetScreenHeight(), w = GetScreenWidth();
     Vector2 dots2D[8] = {GetWorldToScreen(dots3D[0], camera),
     GetWorldToScreen(dots3D[1], camera),
@@ -72,7 +95,7 @@ void DrawCW(Vector3 dots3D[]){
     GetWorldToScreen(dots3D[5], camera),
     GetWorldToScreen(dots3D[6], camera),
     GetWorldToScreen(dots3D[7], camera)};
-    
+
     if (dots2D[0].x <= w && dots2D[0].y <= h &&
     dots2D[1].x <= w && dots2D[1].y <= h &&
     dots2D[2].x <= w && dots2D[2].y <= h &&
@@ -94,6 +117,7 @@ void DrawCW(Vector3 dots3D[]){
     DrawLine(dots2D[3].x, dots2D[3].y,dots2D[7].x, dots2D[7].y,RED);//011-111
     DrawLine(dots2D[6].x, dots2D[6].y,dots2D[7].x, dots2D[7].y,RED);//110-111
     }
+    }
 }
 
 int main()
@@ -104,7 +128,6 @@ int main()
     const int screenHeight = 540;
 
     InitWindow(screenWidth, screenHeight, "raylib");
-    
     camera.position = (Vector3){ 10.0f, 10.0f, 8.0f };
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
@@ -145,13 +168,19 @@ static void UpdateDrawFrame(void)
 {
     UpdateCamera(&camera, CAMERA_FREE);
     //Vector3 *dots3d = getDots(1,1,1);
-    
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+   // Vector3 v1 = camera.position;
+   // Vector3 v2 = camera.target;
+   // Vector3 v12 = (Vector3){camera.target.x - camera.position.x,camera.target.y - camera.position.y,camera.target.z - camera.position.z};
+   // Vector3 v1d = (Vector3){dot3D.x - camera.position.x,dot3D.y - camera.position.y,camera.target.z - camera.position.z};
+    //float ang = acos((float)(v12.x*v1d.x + v12.y*v1d.y + v12.z*v1d.z)/((float)sqrt(v12.x*v12.x + v12.y*v12.y + v12.z*v12.z) * (float)sqrt(v1d.x*v1d.x + v1d.y*v1d.y + v1d.z*v1d.z)))*180/3.1415;
+    }
     BeginDrawing();
 
         ClearBackground(RAYWHITE);
         BeginMode3D(camera);
-            
-            
+            DrawRay(ray[0][0], BLACK);
+            DrawLine3D(v1,v2, BLACK);
             
             //DrawSphere((Vector3){x, y, z},1,RED);  
             //DrawSphereWires((Vector3){x, y, z},1.05,25,25,BLACK);  
@@ -161,7 +190,7 @@ static void UpdateDrawFrame(void)
         for (i = 0; i < 100; i++){
             for (j = 0; j < 100; j++){
                 for (k = 0; k < 100; k++){
-                    if (arr[i][j][k] == 1)
+                    if (arr[i][k][j] == 1)
                     DrawCW(getDots(i,j,k));
                 }
             }
